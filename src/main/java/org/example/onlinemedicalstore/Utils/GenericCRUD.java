816e3393d16f99ba81290621a5f622e54a55dbb7
Maneesha-Prabashwara <mannish1215@gmail.com>
2025-05-14 19:49:36 +0530

    Create GenericCRUD

---
+++ src/main/java/org/example/onlinemedicalstore/Utils/GenericCRUD.java
@@ -0,0 +1,15 @@
+package org.example.onlinemedicalstore.Utils;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.Gson;
+import org.example.onlinemedicalstore.Classes.Customer;
+import org.example.onlinemedicalstore.Classes.Supplier;
+import org.example.onlinemedicalstore.Classes.User;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
