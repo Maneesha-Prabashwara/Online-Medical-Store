db702d5985254d9c3464dae9d010efeefba05026
UthpalaWAD <uthpalakai1@gmail.com>
2025-05-17 13:42:26 +0530

    Create UserFileHandler

---
+++ src/main/java/org/example/onlinemedicalstore/FileHandlers/UserFileHandler.java
@@ -0,0 +1,12 @@
+package org.example.onlinemedicalstore.FileHandlers;
+
+import com.sun.security.auth.login.ConfigFile;
+import org.example.onlinemedicalstore.Classes.User;
+import org.example.onlinemedicalstore.FileConfig.Config;
+import org.example.onlinemedicalstore.Interfaces.IUserFileHandler;
+import org.example.onlinemedicalstore.utils.JsonHelper;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
