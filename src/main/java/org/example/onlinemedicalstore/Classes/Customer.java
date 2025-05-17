d8011bc600d2660790781a567e3af7040a97917e
UthpalaWAD <uthpalakai1@gmail.com>
2025-05-17 09:58:42 +0530

    Create Customer

---
+++ src/main/java/org/example/onlinemedicalstore/Classes/Customer.java
@@ -0,0 +1,27 @@
+package org.example.onlinemedicalstore.Classes;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer extends  User{
+
+    private List<Order> orders;
+
+
+    public Customer(String id ,String name, String username, String password, String role,String email) {
+        super(id ,name, username, password, role,email);
+        this.orders= new ArrayList<>();
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+
+    public Customer(String id, String updatedUsername, String name, String email) {
+        super(id,updatedUsername, name,null,"customer", email);
+        this.orders= new ArrayList<>();
+    }
+    
+}
