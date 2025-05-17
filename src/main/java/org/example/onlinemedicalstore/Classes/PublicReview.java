1e1e0220458d2d19db8eaf5b42cc19ba888e8288
Yesmika <yesmikasewmini@gmail.com>
2025-05-17 10:27:43 +0530

    Create PublicReview

---
+++ src/main/java/org/example/onlinemedicalstore/Classes/PublicReview.java
@@ -0,0 +1,25 @@
+package org.example.onlinemedicalstore.Classes;
+
+public class PublicReview extends Review {
+
+    public PublicReview() {
+        super();
+    }
+
+    public PublicReview(String id, String userId, String productId, String content, int rating) {
+        super(id, userId, productId, content, rating);
+    }
+
+    // This can be used to create a public review from a base review
+    public static PublicReview fromReview(Review review) {
+        PublicReview publicReview = new PublicReview();
+        publicReview.setId(review.getId());
+        publicReview.setUserId(review.getUserId());
+        publicReview.setProductId(review.getProductId());
+        publicReview.setContent(review.getContent());
+        publicReview.setRating(review.getRating());
+        publicReview.setDateCreated(review.getDateCreated());
+        publicReview.setVerified(false);
+        return publicReview;
+    }
+}
