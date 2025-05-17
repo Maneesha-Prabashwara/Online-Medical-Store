7711afa9296c6c2dd3b12e729139449c38c666c1
ShayaniSansala <shayanisansala04@gmail.com>
2025-05-17 11:56:21 +0530

    Create ViewOrder

---
+++ src/main/java/org/example/onlinemedicalstore/Servlets/ViewOrder.java
@@ -0,0 +1,33 @@
+package org.example.onlinemedicalstore.Servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.onlinemedicalstore.Classes.Order;
+import org.example.onlinemedicalstore.FileConfig.Config;
+import org.example.onlinemedicalstore.utils.GenericCRUD;
+
+import java.io.IOException;
+import java.util.List;
+
+
+@WebServlet("/viewOrder")
+public class ViewOrder extends HttpServlet {
+
+    private final String ORDER_FILEPATH = Config.ORDERS.getPath();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        doGet(request, response);
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        GenericCRUD<Order> orderGenericCRUD = new GenericCRUD<Order>(Order.class, ORDER_FILEPATH);
+        List<Order> orderList = orderGenericCRUD.readAll();
+        request.setAttribute("orderlist", orderList);
+        request.getRequestDispatcher("viewOrder.jsp").forward(request, response);
+    }
+}
