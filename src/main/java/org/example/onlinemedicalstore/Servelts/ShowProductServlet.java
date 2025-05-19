1675c7d027718a5531321aaba8a0e9ebe3ed7cc2
sasmitha89 <sasdimanthaj@gmail.com>
2025-05-19 10:30:48 +0530

    Create ShowProductServlet

---
+++ src/main/java/org/example/onlinemedicalstore/Servelts/ShowProductServlet.java
@@ -0,0 +1,41 @@
+package org.example.onlinemedicalstore.Servelts;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.example.onlinemediclestore.Classes.Medicine;
+import org.example.onlinemediclestore.FileConfig.Config;
+import org.example.onlinemediclestore.utils.GenericCRUD;
+
+import javax.print.attribute.standard.MediaName;
+import java.io.IOException;
+import java.util.List;
+
+
+
+@WebServlet("/showProducts")
+public class ShowProductServlet extends HttpServlet {
+    private final String  FILEPATH = Config.UPLOAD_DIR.getPath();
+
+
+
+
+    protected  void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+
+
+        // post method go here
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+
+        GenericCRUD<Medicine> medicineGenericCRUD= new GenericCRUD<>(Medicine.class,Config.MEDICINES.getPath());
+        List<Medicine> medicineList= medicineGenericCRUD.readAll();
+        HttpSession  session= request.getSession(false);
+        request.setAttribute("medicines", medicineList);
+        request.getRequestDispatcher("dashboard.jsp").forward(request, response);
+
+    }
+}
