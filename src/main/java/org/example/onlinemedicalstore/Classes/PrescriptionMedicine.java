2c59bb401f2bf2c1c266d2c659cdad4ca0d3579e
sasmitha89 <sasdimanthaj@gmail.com>
2025-05-19 10:22:51 +0530

    Create PrescriptionMedicine

---
+++ src/main/java/org/example/onlinemedicalstore/Classes/PrescriptionMedicine.java
@@ -0,0 +1,20 @@
+package org.example.onlinemedicalstore.Classes;
+
+public class PrescriptionMedicine extends Medicine{
+    public PrescriptionMedicine(String name, Supplier supplier, String id) {
+        super(name, supplier, id);
+
+    }
+
+    public PrescriptionMedicine(String name, Supplier supplier, String id, String description, double price, int quantity, String imgPath, boolean isAvailable) {
+        super(name, supplier, id, description, price, quantity, imgPath, isAvailable);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Prescription" + super.toString();
+    }
+
+}
+
