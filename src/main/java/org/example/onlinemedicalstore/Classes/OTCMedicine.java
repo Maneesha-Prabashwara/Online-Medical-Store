7479c60f799cb6f206abc82324957833dbac019b
sasmitha89 <sasdimanthaj@gmail.com>
2025-05-19 10:19:41 +0530

    Create OTCMedicine

---
+++ src/main/java/org/example/onlinemedicalstore/Classes/OTCMedicine.java
@@ -0,0 +1,19 @@
+package org.example.onlinemedicalstore.Classes;
+
+public class OTCMedicine extends Medicine{
+
+    public OTCMedicine(String name, Supplier supplier, String id) {
+        super(name, supplier, id);
+    }
+
+
+    public OTCMedicine(String name, Supplier supplier, String id, String description, double price, int quantity, String imgPath, boolean isAvailable) {
+        super(name, supplier, id, description, price, quantity, imgPath, isAvailable);
+    }
+
+
+    @Override
+    public String toString() {
+        return "OTC" + super.toString();
+    }
+}
